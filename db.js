2dea4b678ec2b764f3c6c89d392805af6dad296c
Kriti3009 <kritigoel3009@gmail.com>
2025-06-21 16:23:13 +0530

    ch

--- db.js
+++ db.js
@@ -8,9 +8,16 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME
 });
 
-connection.connect((err) => {
+connection.query(`
+  CREATE TABLE IF NOT EXISTS schools (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL
+  )
+`, (err, results) => {
   if (err) throw err;
-  console.log("MySQL connected...");
+  console.log("Table created");
 });
-
 module.exports = connection;
